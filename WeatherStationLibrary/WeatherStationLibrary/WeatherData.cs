604a60cf5b310ff95c3cd0a5426a093a94d399db
yugnong <yugnong@gmail.com>
2017-06-04 21:42:40 +0800

    update

---
+++ WeatherStationLibrary/WeatherStationLibrary/WeatherData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStationLibrary
+{
+    public class WeatherData
+    {
+        public double Temperature;
+        public double Humidity;
+        public double Pressure;
+
+        public bool Equals(WeatherData other)
+        {
+            return this.Temperature == other.Temperature
+                   && this.Humidity == other.Humidity
+                   && this.Pressure == other.Pressure;
+        }
+    }
+}
